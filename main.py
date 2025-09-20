36cb206b683fae46aa1ab5789ca68768f13840d1
Ted Vtorov <58144503+BUSH222@users.noreply.github.com>
2025-09-21 00:09:38 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+import os
+
+
+def main(in_path='2025-09-20_15-22_proba2_dump_2.235 GHz/SWAP', out_path='results'):
+    # Colorize output
+    os.makedirs(out_path, exist_ok=True)
+    for fname in os.listdir(in_path):
+        if fname.startswith('SWAP_') and fname.endswith('.png'):
+            pass
+
+
 if __name__ == '__main__':
-    pass  # Do something
+    main()
