75e9629b1960a35f11c0c6dd7516fb565dca78ef
Ted Vtorov <58144503+BUSH222@users.noreply.github.com>
2025-09-21 19:52:32 +0300

    stabilized

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 import argparse
+import imageio
 
 
 def find_circle(img):
@@ -29,18 +30,53 @@ def shift_image(img, current_x, current_y, target_x=1024/2, target_y=1024/2):
     translation_matrix = np.float32([[1, 0, shift_x], [0, 1, shift_y]])
     shifted_img = cv2.warpAffine(img, translation_matrix, (img.shape[1], img.shape[0]))
     return shifted_img
-    
+
+
+
+def match_rotation(img_base, img_to_rotate):
+    correlations = []
+    for k in range(4):
+        rotated_img = np.rot90(img_to_rotate, k)
+        res = cv2.matchTemplate(rotated_img, img_base, cv2.TM_CCOEFF_NORMED)
+        score = res[0][0]
+        correlations.append(score)
+    best_k = int(np.argmax(correlations))
+    best_score = correlations[best_k]
+    aligned_img = np.rot90(img_to_rotate, best_k)
+    return aligned_img, best_score
 
 
 def main(in_path, out_path, extra_rotation=0):
+    sample_path = os.path.join(in_path, sorted(os.listdir(in_path), key=lambda x: (len(x), x) if x.endswith('.png') else (float('inf'), 0))[0])
+    sample_image = cv2.imread(sample_path, cv2.IMREAD_UNCHANGED)
     for file_name in sorted(os.listdir(in_path), key=lambda x: (len(x), x)):
         if file_name.endswith('.png'):
             img = cv2.imread(os.path.join(in_path, file_name), cv2.IMREAD_UNCHANGED)
             circles = find_circle(img)
-            processed_img = shift_image(img, circles[0], circles[1])
+            print(circles)
+            centered_img = shift_image(img, circles[0], circles[1])
+            rotated_img, best_rotation = match_rotation(sample_image, centered_img)
+            print(best_rotation)
+            rotated_circles = find_circle(rotated_img)
+            print(rotated_circles)
+            rotated_recentered_img = shift_image(rotated_img, rotated_circles[0], rotated_circles[1])
             if not os.path.exists(out_path):
                 os.makedirs(out_path)
-            cv2.imwrite(os.path.join(out_path, file_name), processed_img)
+            cv2.imwrite(os.path.join(out_path, file_name), rotated_recentered_img)
+
+            # gifs
+
+            out_files = [f for f in sorted(os.listdir(out_path), key=lambda x: (len(x), x)) if f.endswith('.png')]
+            images = []
+            for f in out_files:
+                img = cv2.imread(os.path.join(out_path, f))
+                if img is not None:
+                    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                    images.append(img_rgb)
+
+            if images:
+                gif_path = os.path.join(out_path, "out.gif")
+                imageio.mimsave(gif_path, images, duration=0.1, loop=0)
     pass
 
 
